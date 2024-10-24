6a389c58c78e5812a82112bdef7d60253316e145
Revny <Lsilver205@gmail.com>
2024-10-24 12:01:30 -0300

    done

--- A2-P2-MobileData.py
+++ A2-P2-MobileData.py
@@ -6,16 +6,29 @@
 # then calculate the cost based on that range’s cost. 
 
 #Student #:     
-#Student Name:  
+#Student Name:Lucas  
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
-
-
-
-
-
-
+   
+   totaldata=float(input("how much data did you use?")) #getting the user input for data
+   
+   if totaldata<=200:
+    rateofcharge=20
+    print(rateofcharge, "here is how much mb of data you used")
+    
+   elif totaldata >200 and totaldata <500 : #making it so if they type a number between 200 and 500 it will charge them 0.105
+     finaltotal=rateofcharge=0.105*totaldata
+     print(finaltotal, "here is how much mb of data you used")
+     
+   elif totaldata>=500 and totaldata <1000:
+    finaltotal=rateofcharge=0.110*totaldata 
+    print(finaltotal, "here is how much mb of data you used")
+    
+   elif totaldata>=1000:
+    finaltotal=118
+    print(finaltotal, "you have used over a GB") #changed the print text if they are over 1000mb
+    
 
 
     # YOUR CODE ENDS HERE
